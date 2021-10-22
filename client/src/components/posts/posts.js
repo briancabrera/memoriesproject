039dbcad0d67ed728795f02808bb02791433c579
Brian Cabrera <briancabrer4@gmail.com>
2021-10-22 04:16:11 -0300

    actualizado MapStateToprops por useSelector en Posts.j

--- client/src/components/posts/posts.js
+++ client/src/components/posts/posts.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import Post from './post/post';
 import useStyles from './styles.js'
-import { connect } from "react-redux"
+import { connect, useSelector } from "react-redux"
 import { Grid, CircularProgress } from '@material-ui/core'
 
 const Posts = (props) => {
 
     const classes = useStyles();
+    
+    let state = useSelector(state => state.posts)
 
+    console.log(state)
     return (
-        !props.posts ? <CircularProgress /> : (
+        !state ? <CircularProgress /> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
 
                 {
-                    props.posts.map(post => (
+                    state?.map(post => (
                         <Grid key={post._id} item xs={12} sm={6}>
                             <Post post={post} setCurrentId={props.setCurrentId} />
                         </Grid>
@@ -24,9 +27,11 @@ const Posts = (props) => {
     )
 }
 
-const mapStateToProps = (state) => ({
-    posts: state.posts
-})
+// const mapStateToProps = (state) => ({
+//     posts: state.posts
+// })
 
-export default connect(mapStateToProps, {})(Posts)
+// export default connect(mapStateToProps, {})(Posts)
+
+export default Posts
 
